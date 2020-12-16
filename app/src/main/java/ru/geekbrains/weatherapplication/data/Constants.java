905b97e39bdf3b120a802cc8a90bc6b4e8ada625
Bogatyreva Anastasia <bogatyreva@bpcbt.com>
2020-12-16 22:04:38 +0300

    Some changes in Constants

--- app/src/main/java/ru/geekbrains/weatherapplication/data/Constants.java
+++ app/src/main/java/ru/geekbrains/weatherapplication/data/Constants.java
@@ -1,14 +1,7 @@
 package ru.geekbrains.weatherapplication.data;
 
-import android.content.res.Resources;
-
-import java.util.Arrays;
-import java.util.List;
-
 import ru.geekbrains.weatherapplication.R;
-import ru.geekbrains.weatherapplication.item.OptionItem;
 
-import static ru.geekbrains.weatherapplication.data.SystemPreferences.IS_NIGHT_MODE;
 
 public class Constants {
     public static final String GET_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?";
@@ -23,8 +16,6 @@ public class Constants {
     public static final String ATM_PRESSURE_OPTION = "atm_pressure_options";
     public static final String WIND_OPTION = "atm_pressure_options";
     public static final String HUMIDITY_OPTION = "humidity_options";
-
-    public static final List<OptionItem> settings = Arrays.asList(new OptionItem(IS_NIGHT_MODE, Resources.getSystem().getString(R.string.setting_night_mode), SystemPreferences.getBooleanPreference(IS_NIGHT_MODE)));
 
     public static class LoggerMode {
         public static boolean VERBOSE = true;
