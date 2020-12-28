012219c3896dbdbdd81cdd94ef23fe53a137dec5
Bogatyreva Anastasia <bogatyreva@bpcbt.com>
2020-12-28 18:38:43 +0300

    Added push message feature

---
+++ app/src/main/java/ru/geekbrains/weatherapplication/service/CustomFirebaseMessagingService.java
@@ -0,0 +1,47 @@
+package ru.geekbrains.weatherapplication.service;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import ru.geekbrains.weatherapplication.R;
+
+
+public class CustomFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = CustomFirebaseMessagingService.class.getSimpleName();
+    private int messageId = 0;
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        Log.d(TAG, remoteMessage.getNotification().getBody());
+        String title = remoteMessage.getNotification().getTitle();
+        if (title == null){
+            title = getString(R.string.app_name);
+        }
+        String text = remoteMessage.getNotification().getBody();
+        // создать нотификацию
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "2")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(text);
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(messageId++, builder.build());
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "onNewToken -> token=" + token);
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+    }
+
+}
